2b33b96606a35d6c9c80cf57f0948e079d86a3c2
asadullah48 <asadullahshafique@hotmail.com>
2024-05-27 10:19:50 +0500

    Initial commit

---
+++ ans100.js
@@ -0,0 +1,5 @@
+// Q 100
+// Finds the square root of 144
+var squareRoot = Math.sqrt(144);
+console.log("The square root of 144 is ".concat(squareRoot, "."));
+// This line will show that the square root of 144 is 12.
